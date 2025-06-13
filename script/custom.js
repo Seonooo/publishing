c25cf483be1a122bcd375851e2b25d87343f8dfc
seonho <eedd22@naver.com>
2025-06-13 18:23:00 +0900

    3days

--- script/custom.js
+++ script/custom.js
@@ -7,6 +7,17 @@ $(function () {
     $("section").click(function () {
       $(".mega-navi").stop().slideUp();
     });
+    $(".toggle-pw").click(function () {
+      $(this).toggleClass("bi-eye");
+      var inputType = $(this).hasClass("bi-eye") ? "text" : "password";
+      $(this).siblings("input").attr("type", inputType);
+    });
+    $(".btn-login, .login-member a").click(function () {
+      $(".member-login-overlay").fadeIn();
+    });
+    $(".btn-modal-close").click(function () {
+      $(".member-login-overlay").fadeOut();
+    });
   });
   $(".footer-include").load("/include/footer.html");
 
@@ -44,6 +55,21 @@ $(function () {
     $(this).siblings().removeClass("active");
   });
 
+  // take course
+  $(".cart-item .btn-clear").click(function () {
+    $(this).parents(".cart-item").hide();
+  });
+
+  $(".btn-all-clear").click(function () {
+    $(".cart-item").hide();
+  });
+
+  $(".cart-chk-all").click(function () {
+    $(".check-status .cart-chk").prop("checked", this.checked);
+  });
+
+  // member register
+
   // like
   $(".like").click(function () {
     $(this).toggleClass("active");
@@ -56,5 +82,30 @@ $(function () {
         "<b>종료까지</b> %D 일 <em>%H</em>:<em>%M</em>:<em>%S</em> 남음"
       )
     );
+  });
+
+  // class share
+  $(".btn-sidebar-badge.share").click(function () {
+    $(".class-share-overlay").fadeIn();
+    $("body").addClass("active");
+  });
+  $(".class-share-overlay .btn-modal-close").click(function () {
+    $(".class-share-overlay").fadeOut();
+    $("body").removeClass("active");
+  });
+  $(".share-link").click(function () {
+    $(".copied-link").show();
+  });
+
+  // class detail navigation
+  $(".class-detail-navigation a").click(function () {
+    $(this).addClass("active");
+    $(this).siblings().removeClass("active");
+  });
+
+  // class detail curriculum
+  $(".chapter-title").click(function () {
+    $(this).toggleClass("active");
+    $(this).next().toggle();
   });
 });
